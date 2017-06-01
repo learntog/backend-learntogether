11d858ba9822263aaf5b0db186d3b3dd6e8717c4
Surayez <surayez@yahoo.com>
2017-06-01 14:54:03 +1000

    Updated backend.py

--- backend.py
+++ backend.py
@@ -1,24 +1,16 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://learntogether-a250b.firebaseio.com/')
 
-def fileUpload(firebase,file,user):
+
+def fileReader(firebase, file, user):
     unitFile = open(file, 'r')
     unitList = []
 
-    #converts file into readable format (i.e. List)
     for line in unitFile:
         unitList.append(line)
 
     unitFile.close()
     unitStored = []
-    #converts into usable name
-    # 0 - Unit Code
-    # 2 - Group
-    # 3 - Session
-    # 4 - Day && 5 - Time
-    # 7 - Location
-    #uniStored[4] = group
-    #uniStored[5] = activity
 
     for i in range(1,len(unitList)):
         unitLine = unitList[i].strip().split("\t")
@@ -38,53 +30,50 @@ def fileUpload(firebase,file,user):
         unitStrip.append(unitLine[9])
         unitStored.append(unitStrip)
 
-    print("unitStored",unitStored)
+    unitOut = unitStored
+    unitSimple = []
+    unitSimpleTemp = []
+    for unitRef in unitOut:
+        unitStore = unitRef[0]+"*"+unitRef[1]+"*"+unitRef[2]+"*"+unitRef[3]
+        unitStoreTemp = unitRef[0]+"*"+unitRef[1]+"*"+unitRef[2]+"*"+unitRef[3]+ "*" +unitRef[6]
+        unitSimple.append(unitStore)
+        unitSimpleTemp.append(unitStoreTemp)
+    unit = unitSimple
+    unitTemp = unitSimpleTemp
 
-    unit = []
-    for item in unitStored:
-        unitStore = item[0] + "*" + item[1] + "*" + item[2] + "*" + item[3] + "*" + item[6]
-        unit.append(unitStore)
+    firebase.put("", "users/" + str(user[0]) + "/classes", unitTemp)
 
-    print("unit", unit)
-
-    newUnit = [] #all units from single user
+    newUnit = []
     for p in range(len(unit)):
         newUnit.append([])
 
     for x in range(len(unit)):
-        newUnit[x].append(unitStored[x][0])
-        newUnit[x].append(unitStored[x][4])
-        newUnit[x].append(unitStored[x][5]+unitStored[x][2])
+        newUnit[x].append(unitOut[x][0])
+        newUnit[x].append(unitOut[x][4])
+        newUnit[x].append(unitOut[x][5]+unitOut[x][2])
 
-    firebase.put("","users/"+user+"/classes", unit)
 
-    classes = firebase.get("","/classes/")
-    print("classes", classes)
+    oldUsers = []
+    for n in range(len(unit)):
+        oldUsers.append([])
+    temp = []
 
-    # if classes != None:
-    #     for each in unit:
-    #         if each in classes:
-    #             tempUsers = []
-    #             for x in range(len(unit)):
-    #                 tempUsers.append(firebase.get('', '/classes/' + unit[x] + '/users/'))
-    #                 if user not in tempUsers[x]:
-    #                     tempUsers[x].append(user)
-    #
-    #             print("Temp:   ", tempUsers)
-    #
-    #             dictionary = ""
-    #             for i in range(len(unit)):
-    #                 dictionary = {'location': unitStored[i][3], 'time': unitStored[i][2], 'unitID': unitStored[i][0],
-    #                               'group': unitStored[i][1], 'users': tempUsers[i]}
-    #                 firebase.put("", "/classes/" + str(unit[i]), dictionary)
-    # else:
-    
-    dictionary = ""
+    for x in range(len(unit)):
+        temp.append(firebase.get('','/classes/'+unit[x]+'/users/'))
+        if temp[x] not in oldUsers[x] and temp[x] is not None:
+            oldUsers[x].extend(temp[x])
+
+        if user[0] not in oldUsers[x]:
+            oldUsers[x].extend(user)
+
     for i in range(len(unit)):
-        dictionary = {'location': unitStored[i][3], 'time': unitStored[i][2], 'unitID': unitStored[i][0],'group': unitStored[i][1], 'users': [user]}
-        firebase.put("", "/classes/" + str(unit[i]), dictionary)
+        dictionary = {'location': unitOut[i][3], 'time': unitOut[i][2], 'unitID': unitOut[i][0], 'group':unitOut[i][1],'users':oldUsers[i]}
+        firebase.put("","/classes/"+str(unit[i]),dictionary)
 
+    #End of function
+
+
+### Main
 file = 'timetable-10001000.txt'
-user = "test5"
-
-fileUpload(firebase,file,user)
+user = ["Ishan"]
+fileReader(firebase, file, user)
