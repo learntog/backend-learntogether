e00d3a1ddb2406e7a68931103102564b1621a6cf
Ishan Joshi <ijos0001@student.monash.edu>
2017-06-20 10:37:48 +1000

    Add files via upload

--- seperate_unit_types.py
+++ seperate_unit_types.py
@@ -18,10 +18,29 @@ for cl in classes:
         upload[unit][type] = []
     upload[unit][type].append(cl)
 
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://learntogether-a250b.firebaseio.com/')
+pwd = "ishan4899qwert123"
+import pyrebase
 
-firebase.put('','seperate_unit_list/', upload)
+config = {
+    'apiKey': "AIzaSyBLA6sDqXwnMX6tJayM4dmYCPRzt5CW91M",
+    'authDomain': "learntogether-a250b.firebaseapp.com",
+    'databaseURL': "https://learntogether-a250b.firebaseio.com",
+    'storageBucket': "learntogether-a250b.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+
+
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password("ishan@ishanjoshi.me", pwd)
+print(user)
+
+# Get a reference to the database service
+db = firebase.database()
+
+db.child('seperate_unit_list/').set(upload, user['idToken'])
 
 
 
